5034604b3858bdda52c0ea4d3b66a913b0d58c35
Aran <abarz001@odu.edu>
2022-04-18 18:34:00 -0400

    Bare files

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,25 +1,10 @@
 const Migrations = artifacts.require("Migrations");
 const Beverage = artifacts.require("Beverage");
-const Redeem = artifacts.require("Redeem");
-const extBeverage = artifacts.require("extBeverage");
-const Restock = artifacts.require("Restock");
-const BeverageIFace = artifacts.require("BeverageIFace");
-const RedeemIFace = artifacts.require("RedeemIFace");
 
 module.exports = function(deployer) {
   deployer.deploy(Migrations);
 
-  //Deploy the interface contracts that Redeem.sol and Beverage.sol use
-  deployer.deploy(extBeverage);
-  deployer.deploy(BeverageIFace);
-  deployer.deploy(RedeemIFace);
-
   //Deploy Beverage, then deploy Redeem, passing in Beverage's newly deployed address
   //THEN deploy Restock, passing in Redeem AND Beverage's newly deployed addresses
-  deployer.deploy(Beverage).then(function() {
-  return deployer.deploy(Redeem, Beverage.address);
-  }).then(function(){
-  return deployer.deploy(Restock, Redeem.address, Beverage.address); 
-  });
-
+  deployer.deploy(Beverage)
 };
