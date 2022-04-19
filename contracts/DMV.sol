74330b9e57f61303730e50f7979a4914a18f9d05
Aran <abarz001@odu.edu>
2022-04-19 18:37:33 -0400

    Updated DMV contract to baseline

--- contracts/DMV.sol
+++ contracts/DMV.sol
@@ -1,154 +1,111 @@
 pragma solidity >=0.4.22 <0.6.0;
 
 contract DMV {
-
-    //Variable to hold the msg.sender's address
-    //Set to payable since we may have to return unused deposited funds after purchase 
     address payable public buyer;
-    
-    //Globally creating a price variable
-    //Intended to be used for Beverage price & msg.value requirement
-    uint256 Price;
+    uint256 globalPrice;
 
-  //Struct to model a beverage selection
-  struct Beverages {
-      uint id;
-      string name;
-      uint256 cost;
-      uint lifeTimeCount;
-  }
-  
-  //Struct to hold a user's transaction data
-  //Intended per msg.sender
-  //It tracks the total counts for each drink
-  //It tracks the msg.sender's current purse (balance)
-  struct Purchases{
-    uint256 purse;
-    uint cokePurchaseCount; 
-    uint pepsiPurchaseCount; 
-    uint drpPurchaseCount; 
-    uint dewPurchaseCount;
-  }
-    
-     uint numberOfBeverageOptions = 0;
-  
-  
-    // Mapping for Beverages
-    mapping(uint => Beverages) public beverages;
-    
-    // Mapping for a user (msg.sender)
+    struct Vehicles {
+        uint256 id;
+        string name;
+        uint256 cost;
+        uint256 lifeTimeCount;
+    }
+    struct Purchases {
+        uint256 purse;
+        uint256 scionRenewCount;
+        uint256 civicRenewCount;
+        uint256 viperRenewCount;
+        uint256 ferrariRenewCount;
+    }
+    uint256 numVehicleRegistrations = 0;
+    mapping(uint256 => Vehicles) public vehicles;
     mapping(address => Purchases) public userPurchase;
-  
-   //Constructor function initializes the 4 default drink choices
-   constructor () public {
-        Price = 1000000000000000; //wei... 0.001 Eth
+
+    constructor() public {
+        globalPrice = 1000000000000000; // 0.001 Eth
         buyer = msg.sender;
-        addBeverage("Coke");
-        addBeverage("Pepsi");
-        addBeverage("Dr. Pepper");
-        addBeverage("Mountain Dew");
+        addVehicle("Scion");
+        addVehicle("Civic");
+        addVehicle("Viper");
+        addVehicle("Ferrari");
     }
-    
-    //This function is called by the constructor() and adds a beverage to the beverage mapping
-    //The mapping's key is based off of the incrementor
-    //Each beverage's cost is currently set to 0.001 ETH (1,000,000,000,000,000 wei)
-    //Total purchased count per beverage is initialized to zero
-    function addBeverage (string memory _name) private {
-        numberOfBeverageOptions++;
-        beverages[numberOfBeverageOptions] = Beverages(numberOfBeverageOptions, _name, Price, 0);
+    function addVehicle(string memory _name) private {
+        numVehicleRegistrations++;
+        vehicles[numVehicleRegistrations] = Vehicles(
+            numVehicleRegistrations,
+            _name,
+            globalPrice,
+            0
+        );
     }
-    
-    //This function gets the current global Price value
-    function getPrice () public view returns(uint256){
-        return Price;
+
+    function getGlobalPrice() public view returns (uint256) {
+        return globalPrice;
     }
-    
-    //This function sets the global Price variable with the supplied value
-    function setPrice (uint256 _price) public {
-        Price = _price;
-        setBevCost();
+
+    function setGlobalPrice(uint256 _price) public {
+        globalPrice = _price;
+        setVehicleRegCost();
     }
-    
-    //This function sets the cost of each beverage with the value of the global Price variable
-    function setBevCost() private{
-        beverages[1].cost = Price;
-        beverages[2].cost = Price;
-        beverages[3].cost = Price;
-        beverages[4].cost = Price;
+
+    function setVehicleRegCost() private {
+        vehicles[1].cost = globalPrice;
+        vehicles[2].cost = globalPrice;
+        vehicles[3].cost = globalPrice;
+        vehicles[4].cost = globalPrice;
     }
-    
-    
-    //This function allows the buyer to deposit funds into the contract
-    //The deposit MUST be at least the cost of 1 beverage, which at the start is 0.001ETH
-    //Then it sets the individual user's purse variable to the amount deposited 
-    //We don't want to deposit funds into the contract and have anyone else withdraw from it
-    function deposit() public payable returns (uint256, uint256){
+
+    function deposit() public payable returns (uint256, uint256) {
         require(
-            msg.value >= Price,
-            "Error: Insufficient Deposit (Deposit of current cost required)"
-            );
+            msg.value >= globalPrice,
+            "Error"
+        );
         buyer = msg.sender;
-        
         userPurchase[buyer].purse += msg.value;
         return (address(this).balance, userPurchase[buyer].purse);
     }
-    
-    
-    //subtract the total cost of the current beverage selection from the user's purse
-    function calcPurseBalance (uint256 _totalCost) private{
+
+    function updateUserBalance(uint256 _totalCost) private {
         userPurchase[buyer].purse -= _totalCost;
     }
-    
-    //This is the main selection function; it takes in the beverage ID (1-4) and the quantity
-    //It requires that the msg.sender's avaiable funds be able to cover the quantity
-    //If so, it increments the total number of that beverage purchased (for vendor usage in another contract)
-    //It increases the purchased count for the msg.sender (for redemption in another contract)
-    //And it calculates the purse balance
-    function select (uint _beverageID, uint _count) public payable returns (bool){
+
+    function select(uint256 vehicleID, uint256 _count) public payable returns (bool)
+    {
         buyer = msg.sender;
-   
-        //requires that buyer's balance (purse) can accomodate current purchase count
+
+        //Check balance is enough to cover purchase
         require(
-            userPurchase[buyer].purse >= (_count * beverages[_beverageID].cost),
+            userPurchase[buyer].purse >= (_count * vehicles[vehicleID].cost),
             "Error: Insufficient Funds"
-            );
-        
+        );
+
         require(
-            (_beverageID > 0 && _beverageID <5),
+            (vehicleID > 0 && vehicleID < 5),
             "Error: Invalid Selection"
-            );
-        
-        //variable to hold the total cost of the 
-        uint256 totalCost = _count * beverages[_beverageID].cost;
-        
-        //increase the total number of Cokes ever ordered 
-        beverages[_beverageID].lifeTimeCount += _count;
-        
-        //increase the total number of the respective beverage the buyer has ever ordered
-        if(_beverageID == 1){userPurchase[buyer].cokePurchaseCount += _count; }
-        else if(_beverageID == 2){userPurchase[buyer].pepsiPurchaseCount += _count;}
-        else if(_beverageID == 3){userPurchase[buyer].drpPurchaseCount += _count;}
-        else if(_beverageID == 4){userPurchase[buyer].dewPurchaseCount += _count; }
-    
-        //deduct the purchase from the user's purse
-        calcPurseBalance(totalCost);
-    
+        );
+
+        uint256 totalCost = _count * vehicles[vehicleID].cost;
+
+        vehicles[vehicleID].lifeTimeCount += _count;
+
+        if (vehicleID == 1) {
+            userPurchase[buyer].scionRenewCount += _count;
+        } else if (vehicleID == 2) {
+            userPurchase[buyer].civicRenewCount += _count;
+        } else if (vehicleID == 3) {
+            userPurchase[buyer].viperRenewCount += _count;
+        } else if (vehicleID == 4) {
+            userPurchase[buyer].ferrariRenewCount += _count;
+        }
+        updateUserBalance(totalCost);
         return true;
     }
-    
-    //return the current user's purse back to the user's account
-    //zero out the current user's purse
-    function coinReturn () public payable {
+
+    function coinReturn() public payable {
         buyer = msg.sender;
-        require(
-            userPurchase[buyer].purse > 0,
-            "Error: No funds to return"
-            );
-            
+        require(userPurchase[buyer].purse > 0, "Error: No funds to return");
+
         buyer.transfer(userPurchase[buyer].purse);
         userPurchase[buyer].purse -= userPurchase[buyer].purse;
-    }  
-
-        
-    
+    }
 }
--- src/js/app.js
+++ src/js/app.js
@@ -3,14 +3,14 @@ App = {
   contracts: {},
   account: '0x0',
 
-  init: async function() {
+  init: async function () {
 
-  // Load products.
-    $.getJSON('../vehicles.json', function(data) {
+    // Load vehicles available for registration
+    $.getJSON('../vehicles.json', function (data) {
       var carRow = $('#carRow');
       var carTemplate = $('#carTemplate');
 
-      for (i = 0; i < data.length; i ++) {
+      for (i = 0; i < data.length; i++) {
         carTemplate.find('.panel-title').text(data[i].name);
         carTemplate.find('img').attr('src', data[i].picture);
         carTemplate.find('.vehicle').text(data[i].name);
@@ -26,13 +26,13 @@ App = {
     return App.initWeb3();
   },
 
-  initWeb3: function() {
+  initWeb3: function () {
     if (typeof web3 !== 'undefined') {
-      // If a web3 instance is already provided by Meta Mask.
+      // MetaMask
       App.web3Provider = web3.currentProvider;
       web3 = new Web3(web3.currentProvider);
     } else {
-      // Specify default instance if no web3 instance provided
+      // Default provider if none provided
       App.web3Provider = new Web3.providers.HttpProvider('http://127.0.0.1:9545');
       web3 = new Web3(App.web3Provider);
     }
@@ -41,60 +41,56 @@ App = {
     return App.initContract();
   },
 
-  initContract: function() {
-    $.getJSON("DMV.json", function(dmv) {
-      // Instantiate a new truffle contract from the artifact
+  initContract: function () {
+    $.getJSON("DMV.json", function (dmv) {
+      // Use artificate to instantiate new contract
       App.contracts.dmv = TruffleContract(dmv);
-      // Connect provider to interact with contract
+      // Connect web3 to contract
       App.contracts.dmv.setProvider(App.web3Provider);
 
-      return App.bindEvents();
+      return App.bindButtonsToContractFunctions();
     });
   },
 
 
-  bindEvents: function() {
-    //bind buttons to their functions
+  bindButtonsToContractFunctions: function () {
     $(document).on('click', '.btn-deposit', App.depositEth);
     $(document).on('click', '.btn-return', App.cReturn);
     $(document).on('click', '.btn-select', App.select);
 
-    App.contracts.dmv.deployed().then(function(instance) {
+    App.contracts.dmv.deployed().then(function (instance) {
       var DMV_Wallet = instance;
       $("#dmvAddress").html(DMV_Wallet.address);
       console.log(DMV_Wallet.address);
     });
 
-     // Load account data
-    web3.eth.getCoinbase(function(err, account) {
+    //Loading account data
+    web3.eth.getCoinbase(function (err, account) {
       if (err === null) {
         web3.eth.defaultAccount = account;
         $("#accountAddress").html(web3.eth.defaultAccount);
       }
     });
 
-    //if Metamask changes the user account, reload the page.
-    window.ethereum.on('accountsChanged', function (accounts) {   location.reload(true);  });
-
-    //Update the UI with the current price AND user's purse upon refresh
+    //reload page upon MetaMask account change then update balance / current price
+    window.ethereum.on('accountsChanged', function (accounts) { location.reload(true); });
     App.updatePurse();
     return App.getCost();
   },
 
 
-  updatePurse: function(){
-    var dmvInstance; 
-    //ensure the dmv contract is deployed before trying to access its instance
-    App.contracts.dmv.deployed().then(function(instance){
+  updatePurse: function () {
+    var dmvInstance;
+    //deploy dmv contract then set the instance variable
+    App.contracts.dmv.deployed().then(function (instance) {
       dmvInstance = instance;
-      //call the userPurchase mapping using the supplied user account, send result into THEN
-      dmvInstance.userPurchase(web3.eth.accounts[0]).then(function(userPurse){
-        //parse the integer value of the user's returned purse
-        var balance = parseInt(userPurse[0]);
+
+      dmvInstance.userPurchase(web3.eth.accounts[0]).then(function (userPurse) {
+      var balance = parseInt(userPurse[0]);
         //constant used for computation
         const ether = 1000000000000000000; //1 ether
         //readableBalance var stores the balance keyed to ETH
-        var readableBalance = (balance/ether);
+        var readableBalance = (balance / ether);
         //replace the HTML element with the parsed balance
         $("#accountBalance").html("Your balance: " + readableBalance + " ETH");
         console.log("User's Balance:", readableBalance);
@@ -103,12 +99,12 @@ App = {
 
   },
 
-  depositEth: function() {
-    //variable intended to store the deployed beverage contract
+  depositEth: function () {
+    //store dmv contract instance
     var dmvInstance;
-    
-    //Get the deployed Beverage contract and store the instance in dmvInstance var
-    App.contracts.dmv.deployed().then(function(instance) {
+
+    //Get deployed dmv contract, store instance in dmvInstance
+    App.contracts.dmv.deployed().then(function (instance) {
       dmvInstance = instance;
 
       //This is the amount each deposit transaction will send to the contract using the [Deposit Button]
@@ -120,35 +116,35 @@ App = {
 
       //arrive at the amount of Ether to deposit by calculating ether against the selected multiplier
       var depCoin = (ether * multiplier);
-    
+
       //Execute deposit() as a transaction by sending the depCoin
       //The contract will recognize the buyer as msg.sender 
-      return dmvInstance.deposit({value: depCoin});
-    }).then(function() {
+      return dmvInstance.deposit({ value: depCoin });
+    }).then(function () {
       //call to update the user's current purse display 
       App.updatePurse();
     });
   },
 
 
-  cReturn: function() {
+  cReturn: function () {
     var dmvInstance;
 
-    App.contracts.dmv.deployed().then(function(instance) {
+    App.contracts.dmv.deployed().then(function (instance) {
       dmvInstance = instance;
       //call to coinReturn() reimburses all of the user's deposited amount back to user
       return dmvInstance.coinReturn();
-    }).then(function(){
+    }).then(function () {
       //call to update the user's current purse display 
       App.updatePurse();
     });
-  }, 
+  },
 
 
-  select: function() {
+  select: function () {
     var dmvInstance;
 
-    App.contracts.dmv.deployed().then(function(instance) {
+    App.contracts.dmv.deployed().then(function (instance) {
       dmvInstance = instance;
 
       //set dmvID to the int that corresponds to the select buttons
@@ -156,16 +152,16 @@ App = {
       var dmvID = parseInt($(event.target).data('id'));
       console.log(dmvID);
 
-      //Grab the number of beverages from the dropdown selection
+      //Grab the number of vehicles from the dropdown selection
       //dmvID - 1 because dmvId is keyed to 1-4 and ElementIds are keyed to 0-3
       var element = dmvID - 1;
       var dmvSelect = document.getElementById(element);
       console.log("elementbyID:", dmvSelect);
       var dmvCount = dmvSelect.value;
       console.log("Quantity Selected:", dmvCount);
-    
+
       //Call the select() function THEN update the purse with the local value
-      dmvInstance.select(dmvID, dmvCount).then(function(){
+      dmvInstance.select(dmvID, dmvCount).then(function () {
         //call to update the user's current purse display 
         App.updatePurse();
       });
@@ -174,22 +170,21 @@ App = {
   },
 
 
-  updateCost: function(value){
-    //Update the beverage cost with the supplied value
+  updateCost: function (value) {
+    //Update the vehicle registration cost with the supplied value
     $('.panel-dmv').find('.cost').text(value + "ETH");
   },
 
 
-  getCost: function(){
+  getCost: function () {
     const ether = 1000000000000000000; //1 ether
 
     //Ensure that dmv has been deployed
-    App.contracts.dmv.deployed().then(function(instance) {
+    App.contracts.dmv.deployed().then(function (instance) {
       dmvInstance = instance;
-      //newPrice is pulled from getPrice() and sent to THEN
-      var newPrice = dmvInstance.getPrice().then(function(newPrice){
+      dmvInstance.getGlobalPrice().then(function (newPrice) {
         //readablePrice is just the price keyed to ETH instead of WEI
-        var readablePrice = (newPrice/ether);
+        var readablePrice = (newPrice / ether);
         console.log("Global Price is Currently:", readablePrice);
         //update the Cost on the UI
         App.updateCost(readablePrice);
@@ -198,10 +193,10 @@ App = {
   },
 
 
-}; 
+};
 
-$(function() {
-  $(window).load(function() {
+$(function () {
+  $(window).load(function () {
     App.init();
     document.getElementById("tableOfRegistrations").style.display = 'none';
     document.getElementById("addressContainer").style.display = 'none';
@@ -220,11 +215,11 @@ async function showAccountDetails() {
 
 
 async function showRegistrations() {
-    document.getElementById("accountDetails").style.display = 'none';
-    document.getElementById("tableOfRegistrations").style.display = 'block';
-    document.getElementById("addressContainer").style.display = 'none';
-    document.getElementById("reportSoldVehicleContainer").style.display = 'none';
-    document.getElementById("registerNewVehicleContainer").style.display = 'none';
+  document.getElementById("accountDetails").style.display = 'none';
+  document.getElementById("tableOfRegistrations").style.display = 'block';
+  document.getElementById("addressContainer").style.display = 'none';
+  document.getElementById("reportSoldVehicleContainer").style.display = 'none';
+  document.getElementById("registerNewVehicleContainer").style.display = 'none';
 }
 
 
